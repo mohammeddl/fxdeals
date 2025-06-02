d01766c14e8d3e2a28507bd13ec9233afe316a98
mohammeddl <mohammed_daali2@um5.ac.ma>
2025-06-02 19:50:23 +0100

    chore: initial Spring Boot skeleton

---
+++ src/main/java/com/progressoft/fxdeals/FxdealsApplication.java
@@ -0,0 +1,13 @@
+package com.progressoft.fxdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxdealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxdealsApplication.class, args);
+	}
+
+}
